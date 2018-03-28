0554e2bbe7a65d6b3c4c50e510846e44f10da7e7
Edward Bai <edwardbai31@gmail.com>
2018-03-27 22:10:01 -0700

    Add nav bar component

--- frontend/tawkify.jsx
+++ frontend/tawkify.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Congrats from './congrats';
+
+const Root = ({ store }) => (
+  <Provider store={store}>
+    <HashRouter>
+    </HashRouter>
+  </Provider>
+);
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<Congrats />, root);
+  ReactDOM.render(<Root />, root);
 });
