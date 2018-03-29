de766d49cf7e90399e9bb1a1a432709a9b5416d6
Edward Bai <edwardbai31@gmail.com>
2018-03-28 23:38:51 -0700

    Add basic navbar jsx

---
+++ frontend/components/nav/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class NavBar extends React.component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Tawkify</h1>
+      </div>
+    )
+  }
+}
