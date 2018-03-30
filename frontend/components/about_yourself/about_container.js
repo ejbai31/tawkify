f22b9cf4b0225951c626835a8dce7a80708e1a4b
Edward Bai <edwardbai31@gmail.com>
2018-03-29 21:42:12 -0700

    Add redux/set about component

---
+++ frontend/components/about_yourself/about_container.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import About from './about';
+
+const mapStateToProps = state => ({
+
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);
